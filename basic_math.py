e0bf48612ca197149ad3d4d8720f689534d6a41b
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-09-13 18:20:46 +0000

    initial message

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Christina Ngwa
+# Created on: September 2019
+# This is program calculates  the area and perimeter of a rectangle
+#   with dimensions 5cm x 3cm
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
+
